a5ca87f7fec005af66919f4eab006cf27e0bedaa
Ben <hason.ben2@gmail.com>
2020-10-21 20:01:25 +0300

    added dark and light theme

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { lightTheme, darkTheme } from './Themes/themes.js';
+import { ThemeProvider } from 'styled-components';
+import { useToggleTheme } from './Themes/ToggleComponents/ToggleTheme';
+import Toggeler from './Themes/ToggleComponents/Toggeler.jsx';
+import { GlobalStyles } from './Themes/GlobalStyles.js';
 
 const App = () => {
-  return <div>test</div>;
+  const [theme, themeToggler, mountedComponent] = useToggleTheme();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  if (!mountedComponent) {
+    return <div></div>;
+  }
+
+  return (
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <div className="app">
+        <Toggeler theme={theme} toggleTheme={themeToggler}></Toggeler>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/Components/Themes/themes.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  backgroundColor: '#FFF',
+  textColor: '#363537',
+  toggleBorder: '#FFF',
+  background: '#000',
+};
+
+export const darkTheme = {
+  backgroundColor: '#363537',
+  textColor: '#FAFAFA',
+  toggleBorder: '#6B8096',
+  background: '#000',
+};
