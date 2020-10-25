57e6062026b8d091b6dadf05bb91586d39f84bf2
Ben <hason.ben2@gmail.com>
2020-10-25 13:53:39 +0200

    changed from page-container to page display component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 import { useTheme } from './Themes/useTheme';
 import SearchContainer from '../Containers/SearchContainer';
 import TopBarContainer from '../Containers/TopBarContainer';
-import PageContainer from '../Containers/PageContainer';
 import { Route } from 'react-router-dom';
+import Page from './Page';
 
 const App = () => {
   const [theme, toggleTheme] = useTheme();
   return (
     <div className="app-header">
       <ThemeProvider theme={theme}>
-        <PageContainer>
+        <Page>
           <TopBarContainer toggleTheme={toggleTheme}></TopBarContainer>
           <Route exact path="/">
             <SearchContainer></SearchContainer>
           </Route>
-        </PageContainer>
+        </Page>
       </ThemeProvider>
     </div>
   );
