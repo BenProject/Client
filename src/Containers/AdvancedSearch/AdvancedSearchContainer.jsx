79fdd87f7a51ee0f6c4e8aae66f6498b8c1dadf6
Ben <hason.ben2@gmail.com>
2020-10-28 19:33:02 +0200

    moved button to advancedSearchCOntainer from AdvancedSearchParams

--- src/Containers/AdvancedSearch/AdvancedSearchContainer.jsx
+++ src/Containers/AdvancedSearch/AdvancedSearchContainer.jsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import Button from '../../Components/Buttons/Button';
 import Select from '../../Components/Select/Select';
 import config from '../../Config';
-import { selectEntityTypeLabel } from '../../Constants/labelConstants';
+import {
+  searchButtonLabel,
+  selectEntityTypeLabel,
+} from '../../Constants/labelConstants';
 import { fetchAllCategories } from '../../Services/Ontology/ontology.service';
 import AdvancedSearchParams from './AdvancedSearchParams';
 import './advancedSearch.css';
+import { useHistory } from 'react-router-dom';
 
 export default function AdvancedSearchContainer() {
   const [selected, setSelected] = useState('');
+  const [categories, setCategories] = useState([]);
+  const history = useHistory();
 
   const handleSelected = (event) => {
     setSelected(event.target.value);
   };
-
-  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     fetchAllCategories()
@@ -24,6 +29,11 @@ export default function AdvancedSearchContainer() {
         console.log(err);
       });
   }, []);
+
+  function handleClick(event) {
+    event.preventDefault();
+    history.push('/results/search');
+  }
 
   return (
     <div className="advanced-search-container">
@@ -36,10 +46,22 @@ export default function AdvancedSearchContainer() {
           keysDictionary={config.OntologyToHtml.category}
         ></Select>
       </div>
-
-      <div className="advanced-search-container__params">
-        <AdvancedSearchParams typeId={selected}></AdvancedSearchParams>
-      </div>
+      <form className="advanced-search-container__form-container">
+        <div className="advanced-search-container__params">
+          <AdvancedSearchParams typeId={selected}></AdvancedSearchParams>
+        </div>
+        {selected ? (
+          <div className="advanced-search-container__footer">
+            <Button
+              onClick={handleClick}
+              type="submit"
+              buttonText={searchButtonLabel}
+            ></Button>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </form>
     </div>
   );
 }
--- src/Containers/AdvancedSearch/AdvancedSearchParams.jsx
+++ src/Containers/AdvancedSearch/AdvancedSearchParams.jsx
@@ -4,9 +4,6 @@ import { fetchCategoryById } from '../../Services/Ontology/ontology.service';
 import config from '../../Config';
 import FieldList from '../../Components/FieldList/List';
 import './advancedSearch.css';
-import Button from '../../Components/Buttons/Button';
-import { useHistory } from 'react-router-dom';
-import { searchButtonLabel } from '../../Constants/labelConstants';
 import { getKeyAndValOfObject } from '../../Utils/ObjectUtils';
 import { useDispatch } from 'react-redux';
 import { updateParam } from '../../Redux/Actions/AdvancedSearchActions';
@@ -14,13 +11,6 @@ import { updateParam } from '../../Redux/Actions/AdvancedSearchActions';
 export default function AdvancedSearchParams({ typeId }) {
   const [params, setParams] = useState({ must: [], optional: [] });
   const dispatch = useDispatch();
-
-  const history = useHistory();
-
-  function handleClick(event) {
-    event.preventDefault();
-    history.push('/results/search?q=ben');
-  }
 
   function propertiesArrayToFieldArray(arr) {
     return arr.map((property) => {
@@ -50,23 +40,14 @@ export default function AdvancedSearchParams({ typeId }) {
   if (!params || !typeId) return <div />;
 
   return (
-    <form className="params-container">
-      <div className="params__fields">
-        <div className="fields--must">
-          <FieldList must={true} fields={params.must}></FieldList>
-        </div>
-        <div className="fields--optional">
-          <FieldList must={false} fields={params.optional}></FieldList>
-        </div>
+    <div className="fields">
+      <div className="fields--must">
+        <FieldList must={true} fields={params.must}></FieldList>
       </div>
-      <div className="params__submit">
-        <Button
-          onClick={handleClick}
-          type="submit"
-          buttonText={searchButtonLabel}
-        ></Button>
+      <div className="fields--optional">
+        <FieldList must={false} fields={params.optional}></FieldList>
       </div>
-    </form>
+    </div>
   );
 }
 
