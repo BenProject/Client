cf793eb1f034c41ad037588ccc192b3cfd5e6201
Ben <hason.ben2@gmail.com>
2020-10-29 17:40:07 +0200

    added icon for properties

--- src/Containers/InspectEntity/InspecEntityContainer.jsx
+++ src/Containers/InspectEntity/InspecEntityContainer.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Card, CardHeader } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import IconButton from '../../Components/Buttons/IconButton';
+import InsightsContainer from './Tabs/InsightsContainer';
+import PropertiesContainer from './Tabs/PropertiesContainer';
+import RelationsContainer from './Tabs/RelationsContainer';
+import { Info } from '@material-ui/icons';
 
 export default function InspedEntityContainer() {
   const { entityId, inspectStatus } = useParams();
+  const [currentTab, setTab] = useState(<div />);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (inspectStatus === 'relations') setTab(<RelationsContainer />);
+    else if (inspectStatus === 'properties') setTab(<PropertiesContainer />);
+    else if (inspectStatus === 'insights') setTab(<InsightsContainer />);
+    else return <div></div>;
+  }, [inspectStatus]);
+
   return (
-    <div>
-      entity id: {entityId} {inspectStatus}
-    </div>
+    <Card>
+      <CardHeader
+        action={
+          <IconButton
+            icon={<Info />}
+            onClick={() => history.push(`/entity/${entityId}/properties`)}
+          ></IconButton>
+        }
+      />
+
+      {/* <IconButton></IconButton>
+        <IconButton></IconButton> */}
+      {currentTab}
+    </Card>
   );
 }
---
+++ src/Containers/InspectEntity/Tabs/InsightsContainer.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function InsightsContainer() {
+  return <div>Insights</div>;
+}
